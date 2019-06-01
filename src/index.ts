524dacdbe68182f5a976a3639fdca157c016e9ed
Guillaume St-Pierre <guillaume@manifold.co>
2019-06-01 11:37:59 -0400

    It works

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,27 @@
-import { withUiHook, htm as html } from '@zeit/integration-utils'
+import { withUiHook, htm as html } from '@zeit/integration-utils';
 
 import { completeOAuth } from './controllers/zeitOAuth';
 
-const { ROOT_URL } = process.env;
-
 export default withUiHook(async ({ zeitClient, payload }): Promise<string> => {
-    const metadata = await zeitClient.getMetadata();
-    // const { action, projectId } = payload;
+  let metadata = await zeitClient.getMetadata();
+  // const { action, projectId } = payload;
 
-    if (!metadata.manifoldToken && payload.query.code) {
-        await completeOAuth(payload, zeitClient, metadata);
-    }
+  if (!metadata.manifoldToken && payload.query.code) {
+    await completeOAuth(payload, zeitClient, metadata);
+    metadata = await zeitClient.getMetadata();
+  }
 
-    if (metadata.manifoldToken) {
-        return html`
-			<Page>
-				<P>
-				    Logged in with oauth on Manifold
-				</P>
-			</Page>
-		`;
-    }
-
-    const connectUrl = `${ROOT_URL}/login`;
+  if (metadata.manifoldToken) {
     return html`
-		<Page>
-			<Link href=${connectUrl}>Register Your ZEIT account on Manifold</Link>
-		</Page>
-	`
+      <Page>
+          Logged in with oauth on Manifold
+      </Page>
+    `;
+  }
+
+  return html`
+    <Page>
+      You are not logged in... What?
+    </Page>
+  `;
 });
