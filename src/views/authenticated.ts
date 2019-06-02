90e00369d44fa6ee2c5524c3cb4b174dc814e062
Drew Powers <drew@pow.rs>
2019-06-02 11:02:21 -0600

    Improve design for resource details, dashboard, and provisioning flows

--- src/views/authenticated.ts
+++ src/views/authenticated.ts
@@ -4,6 +4,8 @@ import { RouteParams } from '../api/router';
 import { SELECT_PRODUCT } from '../constants';
 import { $ } from '../utils/currency';
 
+/* eslint-disable prefer-destructuring */
+
 export default async (attrs: RouteParams): Promise<string> => {
   const { payload, client } = attrs;
   const resources = payload.projectId ? await client.getResources(payload.configurationId, payload.projectId) : [];
@@ -17,20 +19,21 @@ export default async (attrs: RouteParams): Promise<string> => {
 
   const resourcesDisplay = !resources.length
     ? htm`
-        <Box marginBottom="1rem">
-          <Notice type="message">
-            You have no resource, yet. Click on "Add a new service" to provision one!
-          </Notice>
-        </Box>
+      <Box marginBottom="1rem" backgroundColor="#fff" border="solid rgb(234, 234, 234) 1px" borderRadius="0.4rem" padding="0.5rem 1rem">
+        You have no resources yet. <Link action="${SELECT_PRODUCT}">Add a new service</Link> to get started.
+      </Box>
       `
     : resources.map(
         (resource: Manifold.Resource): string => {
+          let name = htm`<Link action="${`resource-details-${resource.id}`}">${resource.body.name}</Link>`;
           let button = htm`<Button small action="${`resource-details-${resource.id}`}">View resource</Button>`;
 
           if (resource.state === 'provisioning') {
-            button = htm`<Button small disabled>Preparing...</Button>`;
+            name = resource.body.name;
+            button = htm`<Button small disabled>Preparing…</Button>`;
           } else if (resource.state === 'deprovisioning') {
-            button = htm`<Button small disabled>Deprovisioning...</Button>`;
+            name = resource.body.name;
+            button = htm`<Button small disabled>Deprovisioning…</Button>`;
           }
 
           return htm`
@@ -39,16 +42,18 @@ export default async (attrs: RouteParams): Promise<string> => {
               <FsContent>
                 <Box display="flex" alignItems="center" paddingBottom="1rem" paddingLeft="0.15rem">
                   <Box marginRight="0.5rem" width="10px" height="10px" display="block" background="linear-gradient(237deg, #00FF89 0%, #329DD1 100%)" borderRadius="50%"></Box>
-                  <H2>${resource.body.name}</H2>
+                  <H2>${name}</H2>
                 </Box>
                 ${
                   resource.product && resource.plan
-                    ? htm`<Box display="flex" align-items="center" justify-content="flex-start">
+                    ? htm`<Box display="flex" alignItems="center" justifyContent="flex-start">
                   <Box marginRight="0.5rem" borderRadius="3px" overflow="hidden" width="24px" height="24px">
                     <Img src="${resource.product.logoUrl}" width="100%" height="auto"/>
                   </Box>
                   <Box marginRight="0.5rem">${resource.product.name}</Box>
-                  <Box color="#737373">${resource.plan.name} ${$(resource.plan.cost)}/ mo</Box>
+                  <Box color="#737373">${resource.plan.name} ${$(
+                        resource.plan.cost
+                      )}<Box display="inline" marginLeft="-0.25em">/mo</Box></Box>
                 </Box>`
                     : ''
                 }
@@ -65,18 +70,24 @@ export default async (attrs: RouteParams): Promise<string> => {
   return htm`
     <Page>
       ${notice}
-      <Box display="flex" justifyContent="space-between" marginBottom="1rem">
+        <Box display="flex" justifyContent="space-between" marginBottom="1rem">
         <H1>Manifold services</H1>
         <ProjectSwitcher />
-        <Button action="${SELECT_PRODUCT}" small highlight disabled="${payload.projectId ? 'false' : 'true'}">+ Add a new service</Button>
+        <Button action="${SELECT_PRODUCT}" small highlight disabled="${
+    payload.projectId ? 'false' : 'true'
+  }">+ Add a new service</Button>
       </Box>
-      ${payload.projectId ? resourcesDisplay : htm`
+      ${
+        payload.projectId
+          ? resourcesDisplay
+          : htm`
         <Box marginBottom="1rem">
           <Notice type="message">
             Please select a project to get started with Manifold.
           </Notice>
         </Box>
-      `}
+      `
+      }
     </Page>
   `;
 };
--- src/views/resource-detail.ts
+++ src/views/resource-detail.ts
@@ -2,6 +2,7 @@ import { htm } from '@zeit/integration-utils';
 
 import { DASHBOARD } from '../constants';
 import { RouteParams } from '../api/router';
+import { $ } from '../utils/currency';
 import products from '../data/products';
 
 const { ROOT_URL } = process.env;
@@ -49,49 +50,55 @@ export default async (attrs: RouteParams): Promise<string> => {
     <Page>
       <Fieldset>
         <FsContent>
-          <Box display="flex" justifyContent="space-between">
-            <Box display="flex" justifyContent="stretch">
-              <Box>
-                <Img src="${product.logoUrl}" height="48" width="48"/>
+          <Box display="grid" gridColumnGap="1.5rem" gridTemplateColumns="25% auto">
+            <Box paddingTop="1rem" alignItems="center" display="flex" flexDirection="column" textAlign="center">
+              <Box borderRadius="0.375rem" width="72px" height="72px" overflow="hidden">
+                <Img src="${product.logoUrl}" height="auto" width="100%"/>
               </Box>
-              <Box marginLeft="1.5rem">
-                <H1>${resource.body.name}</H1>
-              </Box>
-            </Box>
-            <Box marginLeft="1.5rem">
+              <P>${product.name}</P>
               <Link href="${ssoUrl}" target="_blank">
-               <Button secondary>Open ${product.name} Dashboard</Button>
+                <Button small>
+                  Open Dashboard︎
+                  <Box alignItems="center" display="inline-flex" fontSize="0.875em" height="1.375em" lineHeight="1" marginLeft="0.1em" justifyContent="center" width="1.375em">↗︎</Box>︎
+                </Button>
               </Link>
             </Box>
-          </Box>
-          <Box borderTop="solid 1px gray" marginTop="1rem" marginBottom="1rem"/>
-          <Box width="250px" marginRight="1rem" marginTop="1rem" textAlign="justified">
-            <Fieldset>
-              <FsContent>
-                <H2>${plan.name}</H2>
-                <P>${plan.cost} $</P>
-                <Box borderTop="solid 1px gray" marginTop="1rem" marginBottom="1rem"/>
-                <Box display="flex" flexDirection="column">
-                  ${plan.features.map(
-                    feature => htm`
-                    <Box marginTop="1rem" display="flex" justifyContent="space-between">
-                      <B>${feature.name}</B>
-                      <Box>${feature.valueName}</Box>
-                    </Box>
-                  `
-                  )}
-                </Box>
-              </FsContent>
-            </Fieldset>
+            <Box>
+              <H1>${resource.body.name}</H1>
+              <Fieldset>
+                <FsContent>
+                  <Box fontSize="0.75rem" textTransform="uppercase" fontWeight="500" letterSpacing="1px" color="#777">
+                    Plan
+                  </Box>
+                  <H2>
+                    ${plan.name}
+                    <Box display="inline" color="#777">${$(
+                      plan.cost
+                    )}<Box display="inline" marginLeft="-0.25em">/mo</Box></Box>
+                  </H2>
+                    ${plan.features.map(
+                      (feature, index) => htm`
+                      <Box backgroundColor="${
+                        index % 2 === 0 ? '#fafafa' : 'transparent'
+                      }" display="grid" gridTemplateColumns="1fr 1fr" padding="0.25rem" paddingTop="0.25rem">                        <B>${
+                        feature.name
+                      }</B>
+                        <Box>${feature.valueName}</Box>
+                      </Box>
+                    `
+                    )}
+                </FsContent>
+              </Fieldset>
+            </Box>
           </Box>
         </FsContent>
         <FsFooter>
-          <Box display="flex" justifyContent="space-between" width="100%">
-            <Box fontSize="14px">
+          <Box display="flex" justifyContent="space-between" alignItems="center" width="100%">
+            <Box fontSize="14px" paddingLeft="0.5rem">
               <Link action="${DASHBOARD}">← Back to my resources</Link>
             </Box>
             <Box marginLeft="1.5rem">
-                <Button action="${`deprovision-${resourceId}`}" warning>Deprovision resource</Button>
+              <Button action="${`deprovision-${resourceId}`}" warning small>Delete resource</Button>
             </Box>
           </Box>
         </FsFooter>
